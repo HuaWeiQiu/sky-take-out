73f5a1b2f40c83b2b960f17011a64c1aa1957c47
HuaWeiQiu <qhw1987752122@163.com>
2024-08-27 23:27:33 +0800

    修改与删除菜品

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -6,6 +6,7 @@ import com.sky.entity.Category;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin/category")
+@Api(tags = "分类管理")
 @Slf4j
 public class CategoryController {
 
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,51 @@
+package com.sky.service;
+
+import com.sky.annotation.AutoFill;
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
+
+import java.util.List;
+
+public interface DishService {
+
+    /**
+     * 新增菜品和口味
+     * @param dishDTO
+     */
+    void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+
+    /**
+     * 菜品的起售和停售状态
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 修改菜品与口味
+     * @param dishDTO
+     */
+    void updateWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 根据菜品id查找菜品数据和口味数据
+     * @param id
+     * @return
+     */
+    DishVO getByIdWithFlavor(Long id);
+}
